6089b43f31d9e2c3af5e00f6ac44ae98ae6d090a
Jayesh Savaliya <112215167@cse.iiitp.ac.in>
2024-11-28 12:47:21 +0530

    mongoerror resolved

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -39,48 +39,12 @@ const profileSchema = new mongoose.Schema({
             trim: true,
         },
     },
-    vehicleDetails: {
-        make: {
-            type: String, 
-            trim: true,
-        },
-        model: {
-            type: String,
-            trim: true
-        },
-        capacity: {
-            type: Number,
-        },
-        licenseNumber: {
-            type: "String",
-            unique: true,
-            trim: true
-        }
-    },
     experienceYears: {
         type: Number,
     }
 });
 
-// // Pre-save middleware to validate vehicleDetails based on accountType
-// profileSchema.pre('save', async function (next) {
-//     if (!this.isModified('userId')) return next();
 
-//     const User = mongoose.model('User');  // Access the User model
-//     const user = await User.findById(this.userId);
 
-//     if (!user) {
-//         return next(new Error('User not found'));
-//     }
-
-//     // If the user is a Driver, ensure vehicleDetails are provided
-//     if (user.accountType === CONFIG.ACCOUNT_TYPE.DRIVER) {
-//         if (!this.vehicleDetails || !this.vehicleDetails.make || !this.vehicleDetails.model || !this.vehicleDetails.licenseNumber) {
-//             return next(new Error('Vehicle details are required for Drivers'));
-//         }
-//     }
-
-//     next();
-// });
 
 module.exports = mongoose.model("Profile", profileSchema);
